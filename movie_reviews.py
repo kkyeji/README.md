8fdf718411b038467f129799db9c9ce4e2c33762
kkyeji <101093705+kkyeji@users.noreply.github.com>
2022-06-01 20:48:54 +0900

    Update movie_reviews.py

--- movie_reviews.py
+++ movie_reviews.py
@@ -21,6 +21,24 @@ for page in range(15):
     html = requests.get(url)
     soup = BeautifulSoup(html.content,'html.parser')
     reviews = soup.find_all("td",{"class":"title"})
+    
+     for review in reviews:
+        sentence = review.find("a",{"class":"report"}).get("onclick").split("', '")[2]
+        if sentence != "":
+            movie = review.find("a",{"class":"movie color_b"}).get_text()
+            score = review.find("em").get_text()
+            review_data.append([movie,sentence,int(score)])
+            need_reviews_cnt-= 1  
+    if need_reviews_cnt < 0:                                         
+        break
+    time.sleep(0.5)
+     
+columns_name = ["movie","sentence","score"]
+with open ( "samples.csv", "w", newline ="",encoding = 'utf8' ) as f:
+    write = csv.writer(f)
+    write.writerow(columns_name)
+    write.writerows(review_data)
 
 
-print(reviews)
+for i in range(len(review_data)):
+  print(review_data[i])
